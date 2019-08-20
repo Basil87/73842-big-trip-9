c422a1827f11150a92747ff4c8c341d6700a0804
Valeksandrov <vasco.alex87@gmail.com>
2019-08-20 18:20:56 +0300

    Личный проект: и ехать и шашечки

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,20 +1,9 @@
-export const createFilterTemplate = () => {
+export const createFilterTemplate = ({title}) => {
   return `<form class="trip-filters" action="#" method="get">
-  <div class="trip-filters__filter">
-    <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-    <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-    <label class="trip-filters__filter-label" for="filter-future">Future</label>
-  </div>
-
-  <div class="trip-filters__filter">
-    <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-    <label class="trip-filters__filter-label" for="filter-past">Past</label>
-  </div>
-
+  ${Array.from(title).map((it) => `<div class="trip-filters__filter">
+    <input id="filter-${it}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${it}" checked>
+    <label class="trip-filters__filter-label" for="filter-${it}">${it.toUpperCase()}</label>
+  </div>`).join(``)}
   <button class="visually-hidden" type="submit">Accept filter</button>
-</form>`;
+  </form>`;
 };
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,5 @@
-export const createNavigationTemplate = () => {
+export const createNavigationTemplate = ({items}) => {
   return `<nav class="trip-controls__trip-tabs  trip-tabs">
-              <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
-              <a class="trip-tabs__btn" href="#">Stats</a>
-            </nav>`;
+    ${Array.from(items).map((title) => `<a class="trip-tabs__btn" href="#">${title}</a>`).join(``)}
+    </nav>`;
 };
