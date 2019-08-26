1ea2bc02dfe1fd682a53b2b12fe24a83aaf9f46d
Valeksandrov <vasco.alex87@gmail.com>
2019-08-26 14:53:01 +0300

    Личный проект: расширяя горизонты

---
+++ src/components/point.js
@@ -0,0 +1,56 @@
+import {util} from '../util.js';
+
+export class Point {
+  constructor({title, icon, dueDate, price, additionalOptions}) {
+    this._title = title;
+    this._icon = icon;
+    this._dueDate = new Date(dueDate);
+    this._price = price;
+    this._additionalOptions = additionalOptions;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = util.createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  getTemplate() {
+    return `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${this._icon}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${this._title}</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">${this._dueDate.getHours()} : ${this._dueDate.getMinutes()}</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">${this._dueDate.getHours() + 1} : ${this._dueDate.getMinutes() + 30}</time>
+          </p>
+          <p class="event__duration">1H 30M</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${this._price}</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          <li class="event__offer">
+          ${Array.from(this._additionalOptions).map((it) => `<span class="event__offer-title">${it[0]}</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">${it[1]}</span>`).slice(``)}
+          </li>
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>`;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import {util} from '../src/util.js';
+
 import {getPoint} from '../src/data.js';
 import {getNavigation} from '../src/data.js';
 import {getFilter} from '../src/data.js';
@@ -6,12 +8,58 @@ import {getRoutInfo} from '../src/data.js';
 import {createRoutTemplate} from '../src/components/rout.js';
 import {createNavigationTemplate} from '../src/components/navigation.js';
 
-import {createFormTemplate} from '../src/components/form.js';
+import {PointEdit} from '../src/components/point-edit.js';
 import {createFilterTemplate} from '../src/components/filter.js';
 import {createContainerTemplate} from '../src/components/container.js';
-import {createCardTemplate} from '../src/components/card.js';
+import {Point} from '../src/components/point.js';
 
 const POINTS_COUNT = 4;
+
+const renderTask = (taskMock) => {
+  const task = new Point(taskMock);
+  const taskEdit = new PointEdit(taskMock);
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      pointsContainer.replaceChild(task.getElement(), taskEdit.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  task.getElement()
+    .querySelector(`.event__rollup-btn`)
+    .addEventListener(`click`, () => {
+      pointsContainer.replaceChild(taskEdit.getElement(), task.getElement());
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+  taskEdit.getElement().querySelectorAll(`input`)
+  .forEach((it) => {
+    it.addEventListener(`focus`, () => {
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+  });
+
+  taskEdit.getElement().querySelectorAll(`input`)
+  .forEach((it) => {
+    it.addEventListener(`blur`, () => {
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+  });
+
+  taskEdit.getElement()
+    .querySelector(`.event`)
+    .addEventListener(`submit`, () => {
+      pointsContainer.replaceChild(task.getElement(), taskEdit.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+  util.render(pointsContainer, task.getElement(), util.position.BEFOREEND);
+};
+
+const pointMocks = new Array(POINTS_COUNT)
+                .fill(``)
+                .map(getPoint);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -22,14 +70,6 @@ const renderFilter = (container) => {
     .fill(``)
     .map(getFilter)
     .map(createFilterTemplate)
-    .join(``));
-};
-
-const renderPoints = (container, count) => {
-  container.insertAdjacentHTML(`beforeend`, new Array(count)
-    .fill(``)
-    .map(getPoint)
-    .map(createCardTemplate)
     .join(``));
 };
 
@@ -57,10 +97,6 @@ const siteTripControlsTitleElement = siteTripControlsElement.querySelector(`h2`)
 renderNavigation(siteTripControlsTitleElement);
 renderFilter(siteTripControlsElement);
 render(siteTripEventsElement, createContainerTemplate(), `beforeend`);
-
-const eventsListElement = siteTripEventsElement.querySelector(`.trip-events__list`);
-
-render(eventsListElement, createFormTemplate(), `beforeend`);
-
-renderPoints(eventsListElement, POINTS_COUNT);
+const pointsContainer = siteTripEventsElement.querySelector(`.trip-events__list`);
+pointMocks.forEach((pointMock) => renderTask(pointMock));
 renderRoutInfo(siteTripInfoElement);
