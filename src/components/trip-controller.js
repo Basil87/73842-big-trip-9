62e01250d5e135a76d21c4d2108314b2e40c5533
Vasiliy Aleksandrov <alex.v.a@inbox.ru>
2019-08-31 17:13:50 +0300

    Личный проект: запомни и пересчитай

--- src/components/trip-controller.js
+++ src/components/trip-controller.js
@@ -8,13 +8,17 @@ export class TripController extends AbstractComponent {
   constructor(container, points) {
     super();
     this.container = container;
+    this.pointContainer = this.container.querySelector(`.trip-events__list`);
     this._points = points;
     this._sort = new Sort();
   }
 
   init() {
-    util.render(this._container, this._sort.getElement(), util.position.BEFOREBEGIN);
+    util.render(this.container, this._sort.getElement(), util.position.AFTERBEGIN);
     this._points.forEach((pointMock) => this._initPoint(pointMock));
+
+    this._sort.getElement()
+    .addEventListener(`click`, (evt) => this._onSortLinkClick(evt));
   }
 
   _initPoint(taskMock) {
@@ -25,14 +29,14 @@ export class TripController extends AbstractComponent {
 
     const onEscKeyDown = (evt) => {
       if (evt.key === `Escape` || evt.key === `Esc`) {
-        this.container.replaceChild(pointElement, pointEditElement);
+        this.pointContainer.replaceChild(pointElement, pointEditElement);
         document.removeEventListener(`keydown`, onEscKeyDown);
       }
     };
 
     pointElement.querySelector(`.event__rollup-btn`)
       .addEventListener(`click`, () => {
-        this.container.replaceChild(pointEditElement, pointElement);
+        this.pointContainer.replaceChild(pointEditElement, pointElement);
         document.addEventListener(`keydown`, onEscKeyDown);
       });
 
@@ -49,11 +53,11 @@ export class TripController extends AbstractComponent {
 
     pointEditElement.querySelector(`.event`)
       .addEventListener(`submit`, () => {
-        this.container.replaceChild(pointElement, pointEditElement);
+        this.pointContainer.replaceChild(pointElement, pointEditElement);
         document.removeEventListener(`keydown`, onEscKeyDown);
       });
 
-    util.render(this.container, pointElement, util.position.BEFOREEND);
+    util.render(this.pointContainer, pointElement, util.position.BEFOREEND);
   }
 
   _onSortLinkClick(evt) {
@@ -63,19 +67,22 @@ export class TripController extends AbstractComponent {
       return;
     }
 
-    this.container.getElement().innerHTML = ``;
+    this.pointContainer.innerHTML = ``;
 
     switch (evt.target.dataset.sortType) {
       case `time`:
-        const sortedByTimeTasks = this._tasks.slice().sort((a, b) => a.dueDate - b.dueDate);
-        sortedByTimeTasks.forEach((taskMock) => this._renderTask(taskMock));
+        const sortedByTimeTasks = this._points.slice().sort((a, b) => a.dueDate - b.dueDate);
+        sortedByTimeTasks.forEach((taskMock) => this._initPoint(taskMock));
+        evt.target.previousElementSibling.checked = true;
         break;
       case `price`:
-        const sortedByPriceTasks = this._tasks.slice().sort((a, b) => a.price - b.price);
-        sortedByPriceTasks.forEach((taskMock) => this._renderTask(taskMock));
+        const sortedByPriceTasks = this._points.slice().sort((a, b) => a.price - b.price);
+        sortedByPriceTasks.forEach((taskMock) => this._initPoint(taskMock));
+        evt.target.previousElementSibling.checked = true;
         break;
       case `event`:
-        this._tasks.forEach((taskMock) => this._renderTask(taskMock));
+        this._points.forEach((taskMock) => this._initPoint(taskMock));
+        evt.target.previousElementSibling.checked = true;
         break;
     }
   }
